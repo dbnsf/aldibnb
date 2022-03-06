1f3985a84c68836b6013da70bfb7c74a027d40b6
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-03-06 13:59:56 +0100

    [UPDATE]Single post style test

--- wordpress/wp-content/themes/aldibnb/single-post.php
+++ wordpress/wp-content/themes/aldibnb/single-post.php
@@ -4,14 +4,14 @@
 
     <?php while (have_posts()) : the_post(); ?>
     <div class="single__container">
-        <div class="img__container">
-        <img src="<?php the_post_thumbnail_url(); ?>" alt="img" class="card-img-top" />
         </div>
         <div class="card-body">
             <h2 class="card-title"><?php the_title(); ?></h2>
-            <?php  echo '<p class="card-content">' . the_content() . '</p>' ?>
             <p class="card-text"><small class="text-muted">Ecrit le : <?php the_date(); ?></small></p>
-        </div>
+
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="img" class="card-img-top" />
+
+            <?php  echo '<p class="card-content">' . the_content() . '</p>' ?>
     </div>
     <?php // If comments are open or we have at least one comment
         if ( comments_open() || get_comments_number() ) :
